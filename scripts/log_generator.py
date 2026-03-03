f1aa2489936095831ea19b48f1a26a4e86140f9b
gravinacoy23 <juangravinacoy@gmail.com>
2026-03-02 23:48:00 -0500

    Implementing function to create directory to put raw logs

--- scripts/log_generator.py
+++ scripts/log_generator.py
@@ -1,5 +1,6 @@
 from datetime import datetime, UTC
 import random
+import os
 
 SERVICES = ["shopping", "pricing", "booking"]
 
@@ -36,6 +37,18 @@ def build_log_line():
     return f'{timestamp} service={service} user={random.randint(1,100)} cpu={random.randint(30,70)} mem={random.randint(40,75)} response={random.randint(200,900)} level={random.choice(MESSAGE_TYPE)} msg="{message}"'
 
 
+def make_directory(base_route):
+    new_dir = "data/raw"
+    joint_route = os.path.join(base_route, new_dir)
+
+    if not os.path.exists(joint_route):
+        os.makedirs(joint_route)
+
+    return joint_route
+
+
 if __name__ == "__main__":
     for _ in range(5):
         print(build_log_line())
+
+    print(make_directory("/home/gravinacoy23/logs-analysis-pipeline/"))
