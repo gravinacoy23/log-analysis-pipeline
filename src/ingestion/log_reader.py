f4e41a01bfe0e3a3716cd68512ee3b38212e7ccc
gravinacoy23 <juangravinacoy@gmail.com>
2026-03-07 12:55:32 -0500

    initial implementation of the log reader with it's documentation

---
+++ src/ingestion/log_reader.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+
+def load_service_logs(service):
+    root_path = Path(__file__).resolve().parents[2]
+    log_path = root_path / "data" / "raw" / service
+
+    if not log_path.is_dir():
+        raise ValueError(f"The service {service} does not exist")
+
+    log_files = [file for file in log_path.iterdir() if file.is_file()]
+
+    if not log_files:
+        raise FileNotFoundError(f"There are no logs for {service}")
+
+    with log_files[0].open("r") as file:
+        data = file.readlines()
+        return data
+
+
+if __name__ == "__main__":
+    log = load_service_logs("booking")
+
+    print(log[0])
