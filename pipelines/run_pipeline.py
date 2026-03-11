56a5d77266bb38039fa7f1ea1af4d9a0b3084316
gravinacoy23 <juangravinacoy@gmail.com>
2026-03-10 19:49:18 -0500

    updated in the pipeline to call the make dataframe function from log_analysis and now returns a pandas DF

--- pipelines/run_pipeline.py
+++ pipelines/run_pipeline.py
@@ -1,12 +1,14 @@
 from src.ingestion.log_reader import load_service_logs
 from src.processing.log_parser import parse_logs
+from src.analysis.log_analysis import convert_to_dataframe
 
 
 def run_pipeline(service):
     logs_list = load_service_logs(service)
     logs_dict = parse_logs(logs_list)
+    logs_dataframe = convert_to_dataframe(logs_dict)
 
-    return logs_dict
+    return logs_dataframe
 
 
 if __name__ == "__main__":
