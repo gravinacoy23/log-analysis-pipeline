a3a04e920f5bbe316ac7c1bae5a76fcc3c7e87b9
gravinacoy23 <juangravinacoy@gmail.com>
2026-03-11 20:43:55 -0500

    initial implementation of the log visualizer function to plot by metric

---
+++ src/analysis/log_visualizer.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+
+
+def plot_metric(metric_dict, metric_name):
+    figure, ax = plt.subplots()
+
+    ax.bar(range(len(metric_dict)), list(metric_dict.values()))
+    ax.set_xticks(range(len(metric_dict)))
+    ax.set_xticklabels(list(metric_dict.keys()))
+    ax.set_title(f"Plot bar for {metric_name.capitalize()} metric")
+    ax.set_xlabel(metric_name)
+    ax.set_ylabel("Occurences")
+
+    return figure
+
+
+if __name__ == "__main__":
+    level_dict = {
+        "INFO": 3,
+        "WARNNING": 2,
+        "ERROR": 1,
+    }
+
+    level_plot = plot_metric(level_dict, "level")
+
+    plt.show()
