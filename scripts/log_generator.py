2a4417759cc8e9594f962642fb578816178d27c2
gravinacoy23 <juangravinacoy@gmail.com>
2026-03-13 14:40:00 -0500

    Changed the name of all the functions except generate_logs() to be private methods

--- scripts/log_generator.py
+++ scripts/log_generator.py
@@ -5,7 +5,7 @@ import yaml
 import argparse
 
 
-def load_config():
+def _load_config():
     parent_path = Path(__file__).resolve().parents[1]
     config_file = parent_path / "config" / "config.yaml"
 
@@ -24,24 +24,24 @@ def load_config():
         return data
 
 
-def generate_log_timestamp():
+def _generate_log_timestamp():
     return datetime.now(tz=UTC).strftime("%Y-%m-%dT%H:%M:%SZ")
 
 
-def generate_runtimestamp():
+def _generate_runtimestamp():
     return datetime.now(tz=UTC).strftime("%Y%m%d_%H%M%S")
 
 
-def generate_service(services):
+def _generate_service(services):
     return random.choice(services)
 
 
-def generate_message(service, message_list):
+def _generate_message(service, message_list):
     service_messages = message_list[service]
     return random.choice(service_messages)
 
 
-def make_raw_directory():
+def _make_raw_directory():
     parent_path = Path(__file__).resolve().parents[1]
     dynamic_dir = parent_path / "data" / "raw"
     dynamic_dir.mkdir(parents=True, exist_ok=True)
@@ -49,36 +49,36 @@ def make_raw_directory():
     return dynamic_dir
 
 
-def make_service_directories(raw_dir, services):
+def _make_service_directories(raw_dir, services):
     for service in services:
         service_dir = raw_dir / service
         service_dir.mkdir(parents=True, exist_ok=True)
 
 
-def write_log(target_path, service, run_timestamp, log_line):
+def _write_log(target_path, service, run_timestamp, log_line):
     log_file = target_path / service / f"{service}_{run_timestamp}.log"
 
     with log_file.open("a", encoding="utf-8") as f:
         f.write(log_line + "\n")
 
 
-def format_log(timestamp, service, user, cpu, memory, response_time, level, message):
+def _format_log(timestamp, service, user, cpu, memory, response_time, level, message):
     return f'timestamp={timestamp} service={service} user={user} cpu={cpu} mem={memory} response_time={response_time} level={level} msg="{message}"'
 
 
-def generate_user():
+def _generate_user():
     return random.randint(1, 100)
 
 
-def generate_memory():
+def _generate_memory():
     return random.randint(40, 75)
 
 
-def generate_cpu():
+def _generate_cpu():
     return random.randint(30, 70)
 
 
-def generate_response_time(cpu):
+def _generate_response_time(cpu):
     if cpu < 50:
         return random.randint(200, 500)
     elif 50 <= cpu < 70:
@@ -87,7 +87,7 @@ def generate_response_time(cpu):
         return random.randint(801, 1200)
 
 
-def determine_level(response_time, levels):
+def _determine_level(response_time, levels):
     if response_time < 600:
         level = random.choices(levels, weights=[1, 0, 0])
         return level[0]
@@ -103,28 +103,28 @@ def _generator_loop(iterations, raw_data, directory, run_timestamp):
     services = raw_data["services"]
     levels = raw_data["levels"]
     messages = raw_data["messages"]
-    make_service_directories(directory, services)
+    _make_service_directories(directory, services)
 
     for _ in range(iterations):
-        timestamp = generate_log_timestamp()
-        service = generate_service(services)
-        message = generate_message(service, messages)
-        user = generate_user()
-        memory = generate_memory()
-        cpu = generate_cpu()
-        response_time = generate_response_time(cpu)
-        level = determine_level(response_time, levels)
-        log = format_log(
+        timestamp = _generate_log_timestamp()
+        service = _generate_service(services)
+        message = _generate_message(service, messages)
+        user = _generate_user()
+        memory = _generate_memory()
+        cpu = _generate_cpu()
+        response_time = _generate_response_time(cpu)
+        level = _determine_level(response_time, levels)
+        log = _format_log(
             timestamp, service, user, cpu, memory, response_time, level, message
         )
-        write_log(directory, service, run_timestamp, log)
+        _write_log(directory, service, run_timestamp, log)
 
 
 def generate_logs(iterations):
-    directory = make_raw_directory()
-    run_timestamp = generate_runtimestamp()
-    raw_data = load_config()
-    make_service_directories(directory, raw_data["services"])
+    directory = _make_raw_directory()
+    run_timestamp = _generate_runtimestamp()
+    raw_data = _load_config()
+    _make_service_directories(directory, raw_data["services"])
 
     _generator_loop(iterations, raw_data, directory, run_timestamp)
 
