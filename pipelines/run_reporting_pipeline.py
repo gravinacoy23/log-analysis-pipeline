bc44ed8773312d86db501f7fc178c792013241fc
gravinacoy23 <juangravinacoy@gmail.com>
2026-03-21 12:50:13 -0500

    Order of methods
    Changed the order of the methods in the file so it's easier to read

--- pipelines/run_reporting_pipeline.py
+++ pipelines/run_reporting_pipeline.py
@@ -3,20 +3,6 @@ from pathlib import Path
 import pandas as pd
 
 
-def _make_output_directory() -> Path:
-    """Makes the directory to save the outputs.
-
-    Returns:
-        Path object with the directory.
-    """
-
-    parent_path = Path(__file__).resolve().parents[1]
-    plot_dir = parent_path / "output" / "plots"
-    plot_dir.mkdir(parents=True, exist_ok=True)
-
-    return plot_dir
-
-
 def report_level_pipeline(logs_dataframe: pd.DataFrame) -> None:
     """Run the pipeline to generate reports and visualizations and save them to the output dir.
 
@@ -30,3 +16,17 @@ def report_level_pipeline(logs_dataframe: pd.DataFrame) -> None:
     count_plot = plot_count_metric(logs_dataframe, "level")
 
     count_plot.savefig(plot_file)
+
+
+def _make_output_directory() -> Path:
+    """Makes the directory to save the outputs.
+
+    Returns:
+        Path object with the directory.
+    """
+
+    parent_path = Path(__file__).resolve().parents[1]
+    plot_dir = parent_path / "output" / "plots"
+    plot_dir.mkdir(parents=True, exist_ok=True)
+
+    return plot_dir
