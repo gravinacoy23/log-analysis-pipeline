ccfa3bf04066fd46daba76f0f56d70d313dbea44
gravinacoy23 <juangravinacoy@gmail.com>
2026-03-29 13:21:12 -0500

    Initial implementation of a bash scrip to execute the pipeline

---
+++ scripts/run_daily.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#Bash script to execute the complete pipeline from the log generation
+#Saves the output of the main.py, includding logger to an execution .log file
+
+set -e
+
+CURRENT_TIME=$(date -u +"%Y%m%d_%H%M%S")
+
+mkdir -p logs/
+
+python3 scripts/log_generator.py -c 2000
+
+python3 main.py -s booking &>> logs/execution_${CURRENT_TIME}.log
