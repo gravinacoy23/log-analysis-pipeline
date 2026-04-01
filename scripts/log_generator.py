3f1f96818e5ed74046eb64b8856b3bd0520b90cd
gravinacoy23 <juangravinacoy@gmail.com>
2026-04-01 07:23:54 -0500

    Reorganizing module
    This module was in a weird order that I used as I was wrtiting it, reorganized so that it can be read in the order it gets executed

--- scripts/log_generator.py
+++ scripts/log_generator.py
@@ -7,6 +7,121 @@ import yaml
 import argparse
 
 
+def generate_logs(iterations: int) -> None:
+    """Orchestrates the log generation calling auxiliary functions to generate
+    the data needed for the loop that generates the logs.
+
+    Args:
+        iterations: number of logs you are generating
+    """
+
+    directory = _make_raw_directory()
+    run_timestamp = _generate_runtimestamp()
+    raw_data = _load_config()
+    _make_service_directories(directory, raw_data["service"])
+    file_handles = _create_files(raw_data["service"], directory, run_timestamp)
+
+    _generator_loop(iterations, raw_data, file_handles)
+
+    _close_files(file_handles)
+
+
+def _generator_loop(
+    iterations: int, raw_data: dict[str, Any], file_handles: dict[str, TextIOWrapper]
+) -> None:
+    """Generates all the log lines for a given number of times and calls the
+    corresponding auxiliary functions to generate format and write the log.
+
+    Args:
+        iterations: Number of logs to be generated.
+        raw_data: contains all the data from the config file
+        file_handles: matches the name of a given service with the file handle
+    """
+
+    services = raw_data["service"]
+    levels = raw_data["level"]
+    messages = raw_data["messages"]
+
+    for _ in range(iterations):
+        timestamp = _generate_log_timestamp()
+        service = _generate_service(services)
+        message = _generate_message(service, messages)
+        user = _generate_user()
+        memory = _generate_memory()
+        cpu = _generate_cpu()
+        response_time = _generate_response_time(
+            cpu, memory, raw_data["metric_thresholds"]
+        )
+        level = _determine_level(response_time, levels)
+        log = _format_log(
+            timestamp, service, user, cpu, memory, response_time, level, message
+        )
+        _write_log(file_handles, service, log)
+
+
+def _make_raw_directory() -> Path:
+    """Makes the directory in which we are going to save the service logs.
+
+    Returns:
+        Path object with the path to the directory for the .log files.
+    """
+
+    parent_path = Path(__file__).resolve().parents[1]
+    dynamic_dir = parent_path / "data" / "raw"
+    dynamic_dir.mkdir(parents=True, exist_ok=True)
+
+    return dynamic_dir
+
+
+def _make_service_directories(raw_dir: Path, services: list[str]) -> None:
+    """Makes a dedicated directory for each service.
+
+    Args:
+        raw_dir: Directory in which we are saving the new dirs.
+        services: Contains the name of all the services.
+    """
+
+    for service in services:
+        service_dir = raw_dir / service
+        service_dir.mkdir(parents=True, exist_ok=True)
+
+
+def _create_files(
+    services: list[str], target_path: Path, run_timestamp: str
+) -> dict[str, TextIOWrapper]:
+    """Creates the files for each service in the dedicated service path and
+    opens it.
+
+    Args:
+        services: All the available services
+        target_path: raw directory where all service directories are
+        run_timestamp: timestamp where all the files got generated
+
+    Returns:
+        A dict that matches the name of the service with the corresponding file handle.
+    """
+
+    file_handles = dict()
+    for service in services:
+        log_file = target_path / service / f"{service}_{run_timestamp}.log"
+        handler = log_file.open("a", encoding="utf-8")
+
+        file_handles[service] = handler
+
+    return file_handles
+
+
+def _close_files(file_handles: dict[str, TextIOWrapper]) -> None:
+    """Closes all the files at the end of the execution.
+
+    Args:
+        file_handles: Dict that matches the name of the service with the corresponding handle.
+    """
+
+    for handle in file_handles.values():
+        handle.close()
+
+
 def _load_config() -> dict[str, Any]:
     """Load constant variables from the config file.
 
@@ -77,114 +192,6 @@ def _generate_message(service: str, message_list: dict[str, list[str]]) -> str:
 
     service_messages = message_list[service]
     return random.choice(service_messages)
-
-
-def _make_raw_directory() -> Path:
-    """Makes the directory in which we are going to save the service logs.
-
-    Returns:
-        Path object with the path to the directory for the .log files.
-    """
-
-    parent_path = Path(__file__).resolve().parents[1]
-    dynamic_dir = parent_path / "data" / "raw"
-    dynamic_dir.mkdir(parents=True, exist_ok=True)
-
-    return dynamic_dir
-
-
-def _make_service_directories(raw_dir: Path, services: list[str]) -> None:
-    """Makes a dedicated directory for each service.
-
-    Args:
-        raw_dir: Directory in which we are saving the new dirs.
-        services: Contains the name of all the services.
-    """
-
-    for service in services:
-        service_dir = raw_dir / service
-        service_dir.mkdir(parents=True, exist_ok=True)
-
-
-def _create_files(
-    services: list[str], target_path: Path, run_timestamp: str
-) -> dict[str, TextIOWrapper]:
-    """Creates the files for each service in the dedicated service path and
-    opens it.
-
-    Args:
-        services: All the available services
-        target_path: raw directory where all service directories are
-        run_timestamp: timestamp where all the files got generated
-
-    Returns:
-        A dict that matches the name of the service with the corresponding file handle.
-    """
-
-    file_handles = dict()
-    for service in services:
-        log_file = target_path / service / f"{service}_{run_timestamp}.log"
-        handler = log_file.open("a", encoding="utf-8")
-
-        file_handles[service] = handler
-
-    return file_handles
-
-
-def _close_files(file_handles: dict[str, TextIOWrapper]) -> None:
-    """Closes all the files at the end of the execution.
-
-    Args:
-        file_handles: Dict that matches the name of the service with the corresponding handle.
-    """
-
-    for handle in file_handles.values():
-        handle.close()
-
-
-def _write_log(
-    file_handles: dict[str, TextIOWrapper], service: str, log_line: str
-) -> None:
-    """Writes the log line in the corresponding file.
-
-    Args:
-        file_handles: Dict that matches the name of the service with the corresponding handle.
-        service: Name of the service
-        log_line: formatted log line ready to go into the .log file
-    """
-
-    handle = file_handles[service]
-
-    handle.write(log_line + "\n")
-
-
-def _format_log(
-    timestamp: str,
-    service: str,
-    user: int,
-    cpu: int,
-    memory: int,
-    response_time: int,
-    level: str,
-    message: str,
-) -> str:
-    """Formats the log line with all the required fields.
-
-    Args:
-        timestamp: timestamp when the log got generated
-        service: from which service is this log
-        user: user in the log
-        cpu: cpu usage at the time
-        memory: mem usage at the time
-        response_time: response time
-        level: info warning or error
-        message: message for the log
-
-    Returns:
-        Complete formatted log line, ready to append to the .log file
-    """
-
-    return f'timestamp={timestamp} service={service} user={user} cpu={cpu} mem={memory} response_time={response_time} level={level} msg="{message}"'
 
 
 def _generate_user() -> int:
@@ -268,56 +275,49 @@ def _determine_level(response_time: int, levels: list[str]) -> str:
         return level[0]
 
 
-def _generator_loop(
-    iterations: int, raw_data: dict[str, Any], file_handles: dict[str, TextIOWrapper]
+def _format_log(
+    timestamp: str,
+    service: str,
+    user: int,
+    cpu: int,
+    memory: int,
+    response_time: int,
+    level: str,
+    message: str,
+) -> str:
+    """Formats the log line with all the required fields.
+
+    Args:
+        timestamp: timestamp when the log got generated
+        service: from which service is this log
+        user: user in the log
+        cpu: cpu usage at the time
+        memory: mem usage at the time
+        response_time: response time
+        level: info warning or error
+        message: message for the log
+
+    Returns:
+        Complete formatted log line, ready to append to the .log file
+    """
+
+    return f'timestamp={timestamp} service={service} user={user} cpu={cpu} mem={memory} response_time={response_time} level={level} msg="{message}"'
+
+
+def _write_log(
+    file_handles: dict[str, TextIOWrapper], service: str, log_line: str
 ) -> None:
-    """Generates all the log lines for a given number of times and calls the
-    corresponding auxiliary functions to generate format and write the log.
+    """Writes the log line in the corresponding file.
 
     Args:
-        iterations: Number of logs to be generated.
-        raw_data: contains all the data from the config file
-        file_handles: matches the name of a given service with the file handle
+        file_handles: Dict that matches the name of the service with the corresponding handle.
+        service: Name of the service
+        log_line: formatted log line ready to go into the .log file
     """
 
-    services = raw_data["service"]
-    levels = raw_data["level"]
-    messages = raw_data["messages"]
+    handle = file_handles[service]
 
-    for _ in range(iterations):
-        timestamp = _generate_log_timestamp()
-        service = _generate_service(services)
-        message = _generate_message(service, messages)
-        user = _generate_user()
-        memory = _generate_memory()
-        cpu = _generate_cpu()
-        response_time = _generate_response_time(
-            cpu, memory, raw_data["metric_thresholds"]
-        )
-        level = _determine_level(response_time, levels)
-        log = _format_log(
-            timestamp, service, user, cpu, memory, response_time, level, message
-        )
-        _write_log(file_handles, service, log)
-
-
-def generate_logs(iterations: int) -> None:
-    """Orchestrates the log generation calling auxiliary functions to generate
-    the data needed for the loop that generates the logs.
-
-    Args:
-        iterations: number of logs you are generating
-    """
-
-    directory = _make_raw_directory()
-    run_timestamp = _generate_runtimestamp()
-    raw_data = _load_config()
-    _make_service_directories(directory, raw_data["service"])
-    file_handles = _create_files(raw_data["service"], directory, run_timestamp)
-
-    _generator_loop(iterations, raw_data, file_handles)
-
-    _close_files(file_handles)
+    handle.write(log_line + "\n")
 
 
 if __name__ == "__main__":
